3f94668a371ee946103dbe3e6b3539ea2a58dcee
Farhad Hormozdiari <fhormoz@Dna-czar.CS.UCLA.EDU>
2013-10-20 12:14:50 -0700

    everythoing works

--- binary.c
+++ binary.c
@@ -3,11 +3,27 @@
 #include <math.h>
 #include <string.h>
 
-#define SMALL 0.05
+#define SMALL 0.01
+
+double rho;
 
 long int fact(int n);
 double likelihoodAllPossibleSet(double * d_configure, double * stat, double * sigma, int size, double NCP, int cut_off );
+double signSubSetLikelihood(double *configure,int one_count, double * stat, double * sigma, int size, double NCP, int cut_off);
 
+void copyConfigure(double *dest, double *src, int size) {
+	int i =0;
+	for(i = 0; i < size; i++) {
+		dest[i] = src[i];
+	}
+}
+
+double min(double a, double b) {
+	if(a>b)
+		return b;
+	else
+		return a;
+}
 
 long int nCr(int n, int r) {
         long int result = 1;
@@ -68,10 +84,8 @@ void multVectorMatrix(double *vector, double * matrix, int size, double * result
 }
 
 double likelihood(double * configure, double * stat, double * sigma, int size, double NCP ) {
-
 	double value1 = 0;
 	double value2 = 0;
-	
 	double * tmpresult = (double *) malloc( sizeof(double *) * size);
 
 	value1 = NCP * multVector(configure, stat, size);
@@ -81,6 +95,29 @@ double likelihood(double * configure, double * stat, double * sigma, int size, d
 	free(tmpresult);
 
 	return (exp(value1) * exp(-value2));
+}
+
+int nextBinarySign(double * data, int size) {
+	int i = 0;
+	int index = 0;
+	int all_neg = 0;	
+	int count_one = 0;
+	double * tmp_data;
+
+	for(i = 0; i < size; i++) {
+		if(data[i] < 0)	all_neg = 1;
+	}
+	if(all_neg == 1) {
+		for(i = 0; i < size; i++)
+			data[i] = -data[i];
+		count_one=nextBinary(data, size);
+		for(i = 0; i < size ; i++)
+			data[i] = -data[i];
+		return(count_one);
+	}
+	else {
+		return(nextBinary(data, size));
+	}
 }
 
 int nextBinary(double * data, int size) {
@@ -187,30 +224,27 @@ int nextBinary2(char * data, int size) {
         return(total_one);
 }
 
-double totalLikelihood(double * stat, double * sigma, int size, double NCP) {	
+double totalLikelihood(double * stat, double * sigma, int size, double NCP, int cut_off) {	
 	long int i = 0;
+	long int j = 0;
+	long int k = 0;
 	double sumLikelihood = 0;
 	double tmp_likelihood = 0;
-	long int total_iteration = nCr(size,0) +  nCr(size,1) +  nCr(size,2) +  nCr(size,3) +  nCr(size,4) +  nCr(size,5);;
-	
-	double * configure = (double *) malloc (size * sizeof(double *));	
-	
+	long int total_iteration = nCr(size,0) +  nCr(size,1) +  nCr(size,2) +  nCr(size,3) +  nCr(size,4) +  nCr(size,5);
+	//long int total_iteration = pow(2,size);
+	double * configure = (double *) malloc (size * sizeof(double *)); // original data	
+
 	for(i = 0; i < size; i++) {
 		configure[i] = 0;
 	}
-
-	i = 0;
-
-	while(i < total_iteration) {
+	for(i = 0; i < total_iteration; i++) {
 		int num = nextBinary(configure, size);
-		tmp_likelihood = likelihood(configure, stat, sigma, size, NCP );
+		tmp_likelihood = signSubSetLikelihood(configure, num, stat, sigma, size, NCP, cut_off)*(pow(SMALL, num))*(pow(1-SMALL, size-num));
+		//printf("==>");
 		//printVector(configure, size);
-		//printf("\t");
-		//printf("%lf\t%lf\tSMALL=%lf\n", tmp_likelihood, pow(SMALL, num)*pow(1-SMALL, size-num), SMALL);
-		sumLikelihood += tmp_likelihood*(pow(SMALL, num))*(pow(1-SMALL, size-num));
-		i++;
+		//printf("\t%lf\n", tmp_likelihood);
+		sumLikelihood += tmp_likelihood;
 	}
-	
 	free(configure);
 	return(sumLikelihood);
 }
@@ -237,77 +271,115 @@ double  addNewSNP(double * configure, double * stat, double * sigma, int size, d
 			}
 		}
 		memcpy(tmp_configure, configure, size * sizeof(double));	
-	}
-	
+	}	
 	memcpy(configure, max_configure, size * sizeof(double));
-
 	free(max_configure);
 	free(tmp_configure);
-
 	return (max_likelihood);
 }
+
+double signSubSetLikelihood(double *configure,int one_count, double * stat, double * sigma, int size, double NCP, int cut_off) {
+	long int i = 0;
+	long int j = 0;
+	long int k = 0;
+	long int total_iteration = 0;
+	double tmp_likelihood = 0;
+	double sumLikelihood  = 0;
+        double * configurecp = (double *) malloc (size * sizeof(double *)); // to generate the sign stuff after each iteration get copy from origniali
+	
+	/*if(cut_off > one_count)
+		total_iteration = pow(2, one_count);
+	else
+		total_iteration = nCr(one_count,0) +  nCr(one_count,1) +  nCr(one_count,2) +  nCr(one_count,3) +  nCr(one_count,4) +  nCr(one_count,5);
+	for(i = 0; i < total_iteration; i++) {*/
+                int num = one_count;
+                memcpy(configurecp, configure, size*sizeof(double));
+                double * signConfigure = (double *) malloc (num * sizeof(double *));
+                for(j = 0; j < num; j++)
+                        signConfigure[j] = 0;
+                int index1 = 0;
+                for(j = 0; j < pow(2,num); j++) {
+                        index1 = 0;
+                        memcpy(configurecp, configure, size*sizeof(double));
+                        //set the configure
+                        for(k = 0; k < size; k++) {
+                                if( (configure[k] == 1) && (signConfigure[index1]==1) ) {
+                                        configurecp[k] = -1;
+                                        index1 = index1 + 1;
+                                }
+                                else if (configure[k]==1) 
+                                        index1 = index1 + 1;
+                        }
+                        tmp_likelihood = likelihood(configurecp, stat, sigma, size, NCP );
+                        //if(tmp_likelihood > 0.01) {
+			//	printVector(configurecp, size);
+			//	printf("\t%lf\n", tmp_likelihood);
+			//}
+			sumLikelihood += tmp_likelihood;
+                        nextBinary(signConfigure, num);
+                }
+                free(signConfigure);
+        //}
+        free(configurecp);
+	return(sumLikelihood);
+}  
 
 double likelihoodAllPossibleSet(double * d_configure, double * stat, double * sigma, int size, double NCP, int cut_off ) {
 	int i = 0;
 	int j = 0;
 	int index = 0;
+	int num = 0;
 	int num_one = 0;
 	int total_sets = 0;
-
 	double total_iteration = 0;
 	double tmp_likelihood = 0;
-
+	double tmp_test = 0;
 	for(i = 0; i < size; i++) {
-		if(d_configure[i] == 1)
+		if(d_configure[i] != 0)
 			num_one++;
 	}	
-	
 	double * tmp_index = (double *) malloc(num_one * sizeof(double *));
 	double * tmp_set   = (double *) malloc(num_one * sizeof(double *));
 	double * tmp_configure = (double *) malloc(size *sizeof(double *));
-
 	for(i = 0; i < size; i++) {
-		if(d_configure[i] == 1) {
+		if(d_configure[i] != 0) {
                         tmp_index[index] = i;
 			index++;
 		}
 	}		
-	for(i = 0; i < num_one; i++) {
+	for(i = 0; i < num_one; i++) 
 		tmp_set[i] = 0;	
-	}
-	
 	index = 0;
-	if(num_one <= cut_off)
-		total_iteration = pow(2, num_one);
-	else
-		total_iteration = nCr(num_one,0) +  nCr(num_one,1) +  nCr(num_one,2) +  nCr(num_one,3) +  nCr(num_one,4) +  nCr(num_one,5);
-	int num = 0;
+	if(num_one <= cut_off)	total_iteration = pow(2, num_one);
+	else			total_iteration = nCr(num_one,0) +  nCr(num_one,1) +  nCr(num_one,2) +  nCr(num_one,3) +  nCr(num_one,4) +  nCr(num_one,5);
 	while(index < total_iteration) {
-		for(j = 0; j < size; j++) {
+		for(j = 0; j < size; j++) 
 			tmp_configure[j] = 0;
-		}
 		for(j = 0; j < num_one; j++) {
-			if(tmp_set[j] == 1)
-				tmp_configure[(int)tmp_index[j]] = 1;
+			if(tmp_set[j] != 0) 
+				tmp_configure[(int)tmp_index[j]] = d_configure[(int)tmp_index[j]];
 		}
-		tmp_likelihood += likelihood(tmp_configure, stat, sigma, size, NCP) * pow(SMALL, num) * pow(1-SMALL, size-num);	
+		tmp_test = signSubSetLikelihood(tmp_configure,num, stat, sigma, size, NCP,cut_off)*(pow(SMALL, num))*(pow(1-SMALL, size-num));	
+		tmp_likelihood += tmp_test;
+		//printf("\t%lf\t%d\t=====\n",tmp_test, num);
 		num = nextBinary(tmp_set, num_one);
 		index++;
 	}
 	free(tmp_configure);
 	free(tmp_index);
 	free(tmp_set);
-
 	return(tmp_likelihood);
 }
 
-double findOptimalSetGreedy(double * stat, double * sigma, int size, double NCP, char * configure) {
+double findOptimalSetGreedy(double * stat, double * sigma, int size, double NCP, char * configure, double inputRho) {
         int i = 0;
+	int index = 0;
         int cut_off = 5;
-
         double tmp_likelihood = 0;
-        double total_likelihood = totalLikelihood(stat, sigma, size, NCP);
+        double total_likelihood = totalLikelihood(stat, sigma, size, NCP,cut_off);
 
+	rho = inputRho;
+	printf("Rho = %lf\n", rho);
         printf("Total = %lf\n", total_likelihood);
 
         double * d_configure = (double *) malloc(size * sizeof(double *));
@@ -321,9 +393,13 @@ double findOptimalSetGreedy(double * stat, double * sigma, int size, double NCP,
         do {
                 tmp_likelihood = addNewSNP(d_configure, stat, sigma, size, NCP, cut_off);
                 printVector(d_configure, size);
-                printf("\n");
-        } while(tmp_likelihood/total_likelihood < 0.95);
+                printf("\t %lf", tmp_likelihood);
+		printf("\n");
+		index++;
+        } while((tmp_likelihood/total_likelihood < rho));
 
+	printVector(d_configure, size);
+	printf("\n");
         for(i = 0; i < size; i++) {
                 if(d_configure[i] == 1) {
                         configure[i] = '1';
@@ -346,11 +422,11 @@ double findOptimalSet(double * stat, double * sigma, int size, double NCP, char 
 	double sumLikelihood = 0;
 	double maxLikelihood = 0;
 
-	double total_likelihood = totalLikelihood(stat, sigma, size, NCP);
+	double total_likelihood = totalLikelihood(stat, sigma, size, NCP, cut_off);
 
 	printf("Total = %lf\n", total_likelihood);
 
-	long int total_iteration = nCr(size,0) +  nCr(size,1) +  nCr(size,2) +  nCr(size,3) +  nCr(size,4) +  nCr(size,5);
+	long int total_iteration = nCr(size,0) +  nCr(size,1) +  nCr(size,2)+nCr(size,3)+nCr(size,4)+nCr(size,5);
 
 	double * d_configure = (double *) malloc(size * sizeof(double *));
 	double * d_max_configure = (double *) malloc (size * sizeof(double *));
@@ -371,7 +447,7 @@ double findOptimalSet(double * stat, double * sigma, int size, double NCP, char 
 			memcpy(d_max_configure, d_configure, size * sizeof(double));	
 		}
 
-		if(tmp_likelihood / total_likelihood >= 0.95) {
+		if(tmp_likelihood / total_likelihood >= rho) {
 			for(j = 0; j < size; j++)
 				if(d_configure[j] == 1) {
 					configure[j] = '1';
@@ -381,13 +457,13 @@ double findOptimalSet(double * stat, double * sigma, int size, double NCP, char 
 		}
                 i++;
         }
-	printf("WE are NEED GREEDY\n");
+	printf("WE are GREEDY\n");
 	
 	do {
 		tmp_likelihood = addNewSNP(d_max_configure, stat, sigma, size, NCP, cut_off);
 		printVector(d_max_configure, size);
 		printf("\n");
-	} while(tmp_likelihood/total_likelihood < 0.95);
+	} while(tmp_likelihood/total_likelihood < rho);
 
 	for(i = 0; i < size; i++) {
 		if(d_max_configure[i] == 1) {
--- calculatePos.py
+++ calculatePos.py
@@ -4,6 +4,7 @@ from math import *
 import random 
 import csv
 import sys
+import optparse
 from math import factorial
 from ctypes import *
 
@@ -86,10 +87,9 @@ def calculate_cutoff_power(N, power, alpha) :
 def pvalues(stat_value) :
 	return(2.0*st.norm.cdf(-abs(stat_value), loc=0, scale=1));
 
-
 def dmvnorm(Z, mu, R, Rinv, Rdet) :
-	[row, col] = Z.shape;
-	return( ( 1 / npy.sqrt(pow(2*pi, col) * abs(Rdet)) )   * exp ( -0.5 * (Z-mu) *  Rinv * (Z-mu).T  ) )
+        [row, col] = Z.shape;
+        return( ( 1 / npy.sqrt(pow(2*pi, col) * abs(Rdet)) )   * exp ( -0.5 * (Z-mu) *  Rinv * (Z-mu).T  ) )
 
 def likelihood_est(configure, Z, R, Rinv, Rdet, NCP) :
 	#mu is the mean for the Multivariance Normal
@@ -117,15 +117,10 @@ def next_binary_value2(curr_binary_value) :
         while(index >= 0 and return_value[0, index] == 1) :
                 index = index - 1;
                 one_countinus_in_end = one_countinus_in_end + 1;
-
-
         #ignore the zeros till you reach the first 1
         while(index >= 0 and return_value[0, index] == 0) :
                 index = index - 1;
-
-
         first_one_in_begin = index;
-
         index1 = 0;
         while(index1 <= size and return_value[0, index1] == 0) :
                 index1 = index1 + 1;
@@ -149,32 +144,21 @@ def next_binary_value2(curr_binary_value) :
 
 def next_binary_value1(curr_binary_value) :
 	[row, col] = curr_binary_value.shape;
-
 	size = col-1;
-
         return_value =  curr_binary_value;
-
         #ignore the end in the end
         index = size;
         one_countinus_in_end = 0
-        
 	while(index >= 0 and return_value[0, index] == 1) :
                 index = index - 1;
                 one_countinus_in_end = one_countinus_in_end + 1;
-       
-
         #ignore the zeros till you reach the first 1
         while(index >= 0 and return_value[0, index] == 0) :
                 index = index - 1;
-     
-
         first_one_in_begin = index;
-
         index1 = 0;
         while(index1 <= size and return_value[0, index1] == 0) :
                 index1 = index1 + 1;
-      
- 
         if (first_one_in_begin > 0 and index1 != index ) :
                 return_value[0, first_one_in_begin] = 0;
                 return_value[0, first_one_in_begin+1] = 1;
@@ -187,7 +171,6 @@ def next_binary_value1(curr_binary_value) :
 		tmp1 = npy.matrix(npy.zeros(shape = (1, size-one_countinus_in_end)));
 		tmp2 = npy.matrix(npy.zeros(shape = (1, one_countinus_in_end+1))+1);
                 return_value = npy.concatenate((tmp2,tmp1),1)
-	
         return(return_value);
 
 # Generate All the possible subset of a set 
@@ -195,11 +178,9 @@ def next_binary_value1(curr_binary_value) :
 # mean the element exist in the subset and 0
 # otherwise
 def subsets(set, N) :
-
         size = len(set);
         #intialize the zero for the container of subsets
         res_subsets = npy.matrix(npy.zeros(shape = (pow(2,size), N)));
-
         index = 0;
         while(index < pow(2,size)) :
                 tmp = decimal2binary(index, size);
@@ -209,9 +190,7 @@ def subsets(set, N) :
                         if(tmp[0,index2] == 1) :
                         	res_subsets[index,set[index2]] = 1; 
                         index2 = index2 + 1;
-               
                 index = index + 1;
-        
         return(res_subsets);
 
 def nCr(n,r):
@@ -219,23 +198,18 @@ def nCr(n,r):
 
 def subsets_cutoff(set, N, cut_off) :
 	size = len(set);
-	
 	index = 0;
 	matrix_size = 0;
-	
 	if(size == 0) :
 		return npy.matrix(npy.zeros(shape = (1, N)));
-
 	if(size >= cut_off) :	
 		while(index <= cut_off) :
 			matrix_size = matrix_size + nCr(size, index);	
 			index = index + 1;
 	else :
 		matrix_size = pow(2,size);	
-
         #intialize the zero for the container of subsets
         res_subsets = npy.matrix(npy.zeros(shape = (matrix_size, N)));
-
         index = 0;
 	cur_binary_value = npy.matrix(npy.zeros(shape = (1, size)));
         while(index < matrix_size-1) :
@@ -249,7 +223,6 @@ def subsets_cutoff(set, N, cut_off) :
 			cur_binary_value[0,0] = 1;
 		elif(index < matrix_size) :
 			cur_binary_value = next_binary_value2(cur_binary_value);
-
         return(res_subsets);
 
 # Help function to generate all the possible subset 
@@ -281,134 +254,151 @@ def all_possible_perm_cutoff(configure, cut_off) :
         sets = subsets_cutoff(set, N+1, cut_off);
         return (sets)
 
-
 def all_possible_perm(configure) :
         set = list();
         index1 = 0;
         [row, col] =  configure.shape;
-
         N = col-1;
-
 	while(index1 <= N) :
                 if(configure[0, index1]==1) :
                         set.append(index1);
                 index1 = index1 + 1;
-     
         sets = subsets(set, N+1);
         return (sets)
 
-
-
-def find_optimal_merge_memory(potential_casual, Z, R, Rinv, Rdet, power, alpha,ro, prob_casual) :
+def find_optimal_merge_memory(potential_casual, Z, R, Rinv, Rdet, power, alpha,ro, prob_casual, est_beta) :
       	index = 0;
 	num1 = 0; 
 	total_sum = 0.0;
         total_casual_cut = 5;
         N = len(potential_casual);
-        NCP = -calculate_cutoff_power(100, power, alpha) * sqrt(100);
-
+	NCP = est_beta;
         print 'NCP=', NCP;
-
 	_binary = CDLL('/home/fhormoz/code/Posterior/libbin.so')
 	data = '0' * N;
-
-        #c_data = c_char_p(data);
         c_size = c_int(N);	
 	c_NCP  = c_double(NCP);
 	c_Z = Z.ctypes.data_as(c_void_p);
 	c_configure = c_char_p(data);
 	vector_R =  npy.reshape(R, N*N);
 	c_total_sum = c_double(0.0);
-	
+	c_ro = c_double(ro);
+	c_R = vector_R.ctypes.data_as(c_void_p);	
 	print vector_R;
-	
-	c_R = vector_R.ctypes.data_as(c_void_p);
-
 	print c_Z;
 	print c_NCP.value;	
-
-	c_total_sum = _binary.findOptimalSetGreedy(c_Z, c_R,  c_size, c_NCP, c_configure);
-
+	c_total_sum = _binary.findOptimalSetGreedy(c_Z, c_R,  c_size, c_NCP, c_configure, c_ro);
 	curr_binary_value = npy.matrix(list(c_configure.value)[0:N], dtype=npy.uint8);
-
 	print c_configure.value;
 	print curr_binary_value;
 	print c_total_sum;
-
-	#while(curr_binary_value.sum() <= total_casual_cut) :
-        #        all_configure = all_possible_perm_cutoff(curr_binary_value, total_casual_cut);
-        #        [row, col] = all_configure.shape;
-	#	index2 = 0;
-        #        tmpvalue = 0;
-        #        while(index2 < row) :
-        #                num1  = all_configure[index2,:].sum();
-        #                prior = prior_cal(prob_casual, N, num1, total_casual_cut);
-        #                tmpvalue = tmpvalue + likelihood_est(all_configure[index2,:], Z, R, Rinv, Rdet, NCP) * prior;
-        #                index2 = index2 + 1;
-        #        if(tmpvalue/total_sum >= ro) :
-        #                return(curr_binary_value);
-        #        curr_binary_value = next_binary_value2(curr_binary_value);
 	print "WE USE HURISTIC FOR HERE"
-	#return(max_value_likelihood_config);
-	#index1 = 0;
-	#index2 = 0;
-	#Z2list = Z.tolist()[0];	# we convert the Z matrix to list to use
-       	#index_Z_sorted = sorted(range(N), key=lambda k: abs(Z2list[k]), reverse=True); 
-	#print index_Z_sorted;
-	#curr_binary_value = npy.matrix(npy.zeros(shape = (1, N)));
-	#tmpvalue = 0;
-	#while(tmpvalue / total_sum < ro and curr_binary_value.sum() <= N*0.3) :
-	#	tmpvalue = 0;
-#		curr_binary_value[0, index_Z_sorted[index1]]  = 1;
-#		all_configure = all_possible_perm_cutoff(curr_binary_value, total_casual_cut);
-#                [row, col] = all_configure.shape;
-#                index2 = 0;
-#                while(index2 < row) :
-#                        num1  = all_configure[index2,:].sum();
-#                        prior = prior_cal(prob_casual, N, num1, total_casual_cut);
-#                        tmpvalue = tmpvalue + likelihood_est(all_configure[index2,:], Z, R, Rinv, Rdet, NCP) * prior;
-#                        if(tmpvalue/total_sum >= ro) :
-#				return curr_binary_value;
-#			#print "step2=", all_configure[index2,:],'\t',tmpvalue, '\t', likelihood_est(all_configure[index2,:], Z, R, NCP) , '\t', prior;
-#                        index2 = index2 + 1;	
-#		index1 = index1 + 1;			
 	return(curr_binary_value);
 
+#computes the conditional causal SNP using iterative procedure
+# if $i$ SNP is picked causal
+# the z-score for the rest is
+# Z_j^{new} = (Z_j^{old} - r_{ij} Z_j)/(sqrt(1-r_{ij}*r_{ij}))
+def conditional_iterative_method(Z, R, causalCount=0) :
+	P = pvalues(Z);
+	Z_copy = Z;
+	print Z.shape;
+	print P;
+	snpcount = Z.shape[0];
+	outList = [];
+	if(causalCount == 0) :
+		while npy.any(P < 0.05/snpcount) :
+			index = numpy.argmax(abs(Z_copy));
+			denominator = npy.subtract(npy.ones(shape=(1,snpcount)), npy.multiply(R[index,:], R[index,:]));
+			denominator[denominator==0] = 1;
+			Z_copy = npy.divide( Z_copy - npy.multiply(R[index,:], Z_copy), npy.sqrt(denominator) );
+			Z_copy = npy.nan_to_num(Z_copy);
+			outList.append(index);
+			P = pvalues(Z_copy);
+			print Z_copy;
+			print outList; 	
+		return(outList);
+	else :
+		while npy.any(len(outList) != causalCount) :
+			index = numpy.argmax(abs(Z_copy));
+			denominator = npy.subtract(npy.ones(shape=(1,snpcount)), npy.multiply(R[index,:], R[index,:]));
+			denominator[denominator==0] = 1;
+			Z_copy = npy.divide( Z_copy - npy.multiply(R[index,:], Z_copy), npy.sqrt(denominator) );
+			Z_copy = npy.nan_to_num(Z_copy);
+			outList.append(index);
+			P = pvalues(Z_copy);
+			print "Z cond=", Z_copy;
+			print "Causal List CM=", outList; 	
+		return(outList);
 
 
-def main(argv):
-	
-	
+#We use the approximation method same as et.al. Nature genetcs
+# In R:
+# p                               # Vector of p-values
+# w      <- exp(.5*qchisq(1-p,1)) # Calculate AIC/BIC weights
+# w      <- w/sum(w)              # Calculate approximate posterior probability
+def approx_posterier_blog(Z,R, causalCount=0) :
+	snps=[];
+	CAP = 80;
+	P = pvalues(Z);
+	snpcount = Z.shape[0];
+	w = st.chi2.ppf(npy.subtract(npy.ones(shape=(1,snpcount)), P),1);
+	print w;
+	w[npy.isinf(w)] = CAP
+	tmp = [0.5]*snpcount;
+	w = npy.exp(npy.multiply(npy.matrix(tmp), w));
+	w = npy.divide(w, npy.sum(w));
+	tmp = [-1]*snpcount;
+	negw = npy.multiply(npy.matrix(tmp), w);
+	print "w2=",w; 	
+	index=npy.argsort(negw);
+	sum = 0;
+	if(causalCount==0) :
+		for value in  numpy.array(index)[0]:
+			sum = sum + w[0,value];
+			snps.append(value);
+			if(sum > 0.95) :
+				print snps;
+				return snps;
+	else:
+		for value in  numpy.array(index)[0]:
+                        sum = sum + w[0,value];
+                        snps.append(value);
+                        if(len(snps) == causalCount) :
+                                print snps;
+                                return snps;
+		
+
+
+
+def main(parser):
 	row = 1;
 	col = 1;
-	ro  = 0.95;
 	power = 0.5;
 	alpha = 1e-08;
-	prob_casual = 2/35;	
+	prob_casual = 1/35;	
 	casual_implant = 1;
 	total_snp = 0;	
-
 	NCP = calculate_cutoff_power(100, power, alpha) * sqrt(100);
-
 	print NCP;
+	#Parse the input
+	(options, args) = parser.parse_args();
+	outputFile = options.outfile;
+	casual_implant =  int(options.causal_count);
+	ldFile =  options.ldfile;
+	est_beta = NCP * float(options.beta_fraction);
+	ext_flag = int(options.ext);	#0 means use original method and 1 means use our extention model
+	rho = float(options.rho)
 
-	#oldR = npy.random.random_sample(size=(row,row));
-	#R1 = (oldR + oldR.T)/2;
-	#R = (R1 * R1.T);
-	#R = nearPD(R, 10);
-	
-	inputFile = open('/home/fhormoz/code/Posterior/data/EUN_MIN_MAX/min.ld', 'rb');
+	inputFile = open(ldFile, 'rb');
 	for data in inputFile:
 		total_snp = total_snp + 1;
 	R = npy.matrix(npy.zeros(shape = (total_snp, total_snp)));	
 	inputFile.close();	
-
 	row = 0;
 	col = 0;
-	
 	print total_snp;
-
-	inputFile = open('/home/fhormoz/code/Posterior/data/EUN_MIN_MAX/min.ld', 'rb');
+	inputFile = open(ldFile, 'rb');
 	for data in inputFile:
         	line = data.split(' ');
 		col = 0;
@@ -417,47 +407,37 @@ def main(argv):
 				R[row, col] = float(l) ;
 			col = col + 1;
 		row = row + 1;
-
+	R = R[1:15,1:15];	
 	[row,col] = R.shape;
 	print row;
 	print col;
-	for command in argv:
-		print command;
-
-	#Rinv = npy.linalg.inv(R);
-	#Rdet = npy.linalg.det(R);
-
 	Rinv = R;
 	Rdet = npy.linalg.det(R);	
-
 	#TODO
 	# Generate the casual SNP
         casual_snp = random.sample(xrange(row), casual_implant);
-	#casual_snp = [24,28]
 	true_casual = [0] * row;
 	for x in casual_snp:
                 true_casual[x] = 1;
-	
 	Z = npy.random.multivariate_normal((NCP * npy.matrix(true_casual) * R).tolist()[0], R);	
-
 	print true_casual;
-
-	pred_casual = find_optimal_merge_memory(range(row), npy.matrix(Z), R, Rinv, Rdet, power, alpha, ro, prob_casual).tolist()[0];
-
-	error = pred_casual - npy.matrix(true_casual);
-	
-	[erow, ecol] = error[error < 0].shape;
+	pred_casual = [];
+	pred_casual = find_optimal_merge_memory(range(row), npy.matrix(Z), R, Rinv, Rdet, power, alpha, rho, prob_casual, est_beta).tolist()[0];
+	pred_casual = [(-1 if num==2 else num) for num in pred_casual]
+	error = 0;
+	for x in range(0,row-1):
+		if((true_casual[x] != 0) & (true_casual[x] != abs(pred_casual[x]))):
+			error = error + 1;
 	print true_casual;
-	print pred_casual;
-	print "Total Error", ecol; 
-	print "Total SNP picked", sum(pred_casual);
-
-	one_indexs = [i for i,val in enumerate(true_casual) if val==1];
+	#print pred_casual;
+	print "Total Error", error; 
+	print "Total SNP picked", str(sum([abs(x) for x in pred_casual]));
+	one_indexs = [i for i,val in enumerate(true_casual) if val!=0];
 	print one_indexs;
 
-	outputFile = open(argv[0],'w');
+	outputFile = open(outputFile,'w');
 	#write the total SNP picked
-	outputFile.write(str(sum(pred_casual)));
+	outputFile.write(str(sum([abs(x) for x in pred_casual])));
 	outputFile.write("\n");
 	#write the set of SNPs picked
 	write2File(outputFile, map(int, pred_casual));
@@ -472,23 +452,56 @@ def main(argv):
 		outputFile.write(str(sort_Z.index(abs(Z[i]))+1));
 		outputFile.write(' ');
 	outputFile.write('\n');	
-	#write the condtional restls
-	outputFile.write("1 2 3 4 5 6\n");	
+	#write the condtional results
+	if(ext_flag==1) :
+		rank_cond = conditional_iterative_method(Z,R, sum(pred_casual));	
+	else :
+		rank_cond = conditional_iterative_method(Z,R);
+	cond_causal = [0]*row;  
+	for value in rank_cond:
+		cond_causal[value] = 1;
+	write2File(outputFile, cond_causal);	
+	outputFile.write("\n");
+	print cond_causal;
+	#wrote the posterier method
+	if(ext_flag == 1):
+		rank_pos = approx_posterier_blog(Z,R, sum(pred_casual));
+	else:
+		rank_pos = approx_posterier_blog(Z,R);
+	pos_causal = [0] *row;
+	for value in rank_pos:
+		pos_causal[value] = 1;
+	write2File(outputFile, pos_causal);
+        outputFile.write("\n");
 	#write the Z score
 	write2File(outputFile, Z);
 	outputFile.write("\n");
 	write2File(outputFile, convertZ2Pvalue(Z));
 	outputFile.write("\n");
-	#write if we have enough power 0 not enough power 1 enogh power
-	#outputFile.write('0' if len(Z>=NCP)==0 else '1');	
+	#write if we have enough power 1, not enough power 0
+	#outputFile.write('1' if len(Z>=NCP)==0 else '1');	
 	power_flag = 0;
-	for val in Z:
-		if(abs(val) >= NCP) :
-			power_flag = 1;
+	if(any(abs(Z) >= NCP)) :
+		power_flag = 1;
 	outputFile.write(str(power_flag));
-	outputFile.write("\n");
-	
+	outputFile.write("\n");	
 	outputFile.close();
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+	parser = optparse.OptionParser("usage: %prog [options] ")
+	parser.add_option("-o", "--out", dest="outfile",
+		default="out", type="string",
+		help="specify the output file")
+	parser.add_option("-l", "--ld_file", dest="ldfile", default="",
+		type="string", help="the ld input file")
+	parser.add_option("-c", "--causal_count", dest="causal_count", default=1,
+                type="int", help="the number of causal SNP implanted in the region")
+	parser.add_option("-b", "--beta_est", dest="beta_fraction", default=1,
+                type="float", help="the fraction of true beta use to set our estimate beta")
+	parser.add_option("-p", "--rho-prob", dest="rho", default=0.95,
+                type="float", help="set $\rho$ probability");
+	parser.add_option("-e", "--extenstion-compare", dest="ext", default=0,
+                type="float", help="if 1 we will run the extention of the conditional and posterier with our number of causal")
+	
+	
+	main(parser);
